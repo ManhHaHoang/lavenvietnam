a27c2fe6527d67dfefed5b04904f596dce5aa976
Manh Ha Hoang <manhha.design@gmail.com>
2021-11-22 23:49:08 +0700

    banner

--- common/home/banner.php
+++ common/home/banner.php
@@ -3,7 +3,7 @@
         <div class="menu-dummy"></div>
         <div class="banner-area flex-b align-s">
             <div class="main-banner">
-                <?php for ($i=0; $i < 4; $i++) { ?>
+                <?php for ($i=0; $i < 7; $i++) { ?>
                     <div class="main-banner-item">
                         <a href="#">
                             <img src="/images/banner/<?=$i?>.png" alt="">
@@ -13,10 +13,10 @@
             </div>
             <div class="small-banner flex-b column flex-s">
                 <div class="small-banner-item">
-
+                    <a href="#"><img src="/images/banner/s1.png" alt=""></a>
                 </div>
                 <div class="small-banner-item">
-
+                    <a href="#"><img src="/images/banner/s2.png" alt=""></a>
                 </div>
             </div>
         </div>
