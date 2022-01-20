ad7c5188564e31e242c77bae6034c4ff72d44a87
Le Van Quan <quanbka.cntt@gmail.com>
2022-01-20 09:27:08 +0700

    fix

--- danh-muc.php
+++ danh-muc.php
@@ -22,8 +22,225 @@
     <section id="js-category">
         <div class="site-w">
             <div class="category-wrapper flex-b align-s">
-                <?php include 'common/products/sidebar.php'; ?>
-                <?php include 'common/products/categories.php'; ?>
+                <aside class="sidebar-area">
+                    <div class="cart-top mobile ">
+                        <div class="mini-cart-heading flex-b align-c">
+                            Lọc sản phẩm
+                        </div>
+                        <span class="closecart closefilter">Đóng</span>
+                    </div>
+                    <div class="sidebar-component">
+                        <div class="sidebar-component-heading">
+                            Danh mục
+                        </div>
+                        <div class="sidebar-component-content">
+                            <div class="filter-lable active">
+                                <a class="filter-link active" href="#/danh-muc-con">
+                                    Máy đo huyết áp
+                                    (9)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Cân sức khỏe
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Máy đo đường huyết
+                                    (1)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Máy tăm nước
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Máy trợ thính
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Vớ y khoa
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Máy massage
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Tinh dầu xông phòng
+                                    (0)
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sidebar-component">
+                        <div class="sidebar-component-heading">
+                            Xuất xứ
+                        </div>
+                        <div class="sidebar-component-content">
+                            <div class="filter-lable active">
+                                <a class="filter-link active" href="#/danh-muc-con">
+                                    Nhật Bản
+                                    (9)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Việt BNam
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Thụy Sỹ
+                                    (1)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Đức
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Italy
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Trung Quốc
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Anh
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Singapore
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Ba Lan
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="#/danh-muc-con">
+                                    Hàn Quốc
+                                    (0)
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sidebar-component">
+                        <div class="sidebar-component-heading">
+                            Khoảng giá
+                        </div>
+                        <div class="list-brand-check">
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="/laptop-cu?max=5000000">Dưới 5 triệu
+                                    (0)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="/laptop-cu?min=5000000&amp;max=10000000">5 triệu - 10 triệu
+                                    (2)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="/laptop-cu?min=10000000&amp;max=15000000">10 triệu - 15 triệu
+                                    (5)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="/laptop-cu?min=15000000&amp;max=20000000">15 triệu - 20 triệu
+                                    (10)
+                                </a>
+                            </div>
+                            <div class="filter-lable ">
+                                <a class="filter-link " href="/laptop-cu?min=20000000">Trên 20 triệu
+                                    (12)
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </aside>
+                <span class="filter-backgroud mobile"></span>
+
+                <div class="category-area">
+                    <article class="category-top-nav flex-b align-c">
+                        <h1 class="category-title">
+                            Thiết bị chăm sóc sức khỏe
+                        </h1>
+                        <form class="category-sort flex-b align-c flex-e" action="<?= $url ?>" method="get">
+                            <button type="submit" name="sort" value="bestsale" class="button sort-item active desktop"> Mua nhiều </button>
+                            <button type="submit" name="sort" value="newest" class="button sort-item desktop"> Mới nhất </button>
+                            <button type="submit" name="sort" value="price-desc" class="button sort-item desktop"> Giá tăng dần </button>
+                            <button type="submit" name="sort" value="price-asc" class="button sort-item desktop"> Giá giảm dần </button>
+                            <span class="button sort-item filter-icon mobile"> Bộ lọc </span>
+                        </ desktop>
+                    </article>
+                    <?php if (key_exists('s', $_GET) && $_GET['s'] == true) { ?>
+                        <article class="filter-result-wrapper flex-b align-c">
+                            <button class="button filter-result flex-b align-c flex-c">
+                                <span>Navy</span>
+                                <small>×</small>
+                            </button>
+                            <a href="danh-muc" class="button sort-item delete-filter"> Xóa </a>
+                        </article>
+                    <?php } ?>
+                    <article class="list-child-category sub-category">
+                        <?php for ($i=1; $i < 9; $i++) { ?>
+                            <div class="sub-category-item">
+                                <a class="sub-category-image" href="?may-do-huyet-ap-c318">
+                                    <img src="/images/category/<?= $i ?>.jpg" alt="">
+                                </a>
+                                <h2 class="child-category">
+                                    <a class="child-category-link" href="?may-do-huyet-ap-c318">Danh mục con (53) </a>
+                                </h2>
+                            </div>
+                        <?php } ?>
+                    </article>
+                    <article class="list-product-item">
+                        <?php for ($i=1; $i < 17; $i++) {
+                            include 'common/products/product-item.php';
+                        } ?>
+                    </article>
+                    <article class="pagination-box">
+                        <div class="pagination flex-b align-c flex-c">
+                            <a class="disabled" href="#/">&laquo;</a>
+                            <a class="current" href="/page=1">1</a>
+                            <a href="?page=2">2</a>
+                            <a href="?page=3">3</a>
+                            <a href="?page=4">4</a>
+                            <a href="?page=5">5</a>
+                            <a href="/page=6">&raquo;</a>
+                        </div>
+                    </article>
+                    <article class="category-description">
+                        Thiết bị y tế Laven Việt Nam cung cấp các sản phẩm thiết bị y tế gia đình như máy đo huyết áp, máy đo đường huyết, cân sức khỏe, nhiệt kế chính hãng, Chất lượng Châu Âu
+                    </article>
+                </div>
+
             </div>
         </div>
     </section>
